9eb423b687d61b27de0a8b268041ed2648d2b266
yjapan2020@gmail.com <yjapan2020@gmail.com>
2023-09-04 01:36:07 +0900

    Add project files.

---
+++ AzureADTest2/Controller/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureADTest2.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Callback([FromForm] string code, [FromForm] string state)
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Redirect("/");
+            } else
+            {
+                return Redirect("/");
+            }
+            
+        }
+    }
+}
